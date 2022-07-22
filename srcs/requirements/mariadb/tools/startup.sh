df686a3d82d1ae85a694627aa037c38b0a32a904
curtm4n <curtman.73@gmail.com>
2022-07-22 16:16:13 +0200

    Automatisation de la creation de la db et des users si non existants

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -3,3 +3,9 @@
 mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 mkdir /run/mysqld
 mariadbd --user=root
+mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
+mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' with grant option"
+mariadb -e "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
+mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' with grant option"
+mariadb -e "FLUSH PRIVILEGES"
