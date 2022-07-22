568667741a2112545a2986f93b29abb271639111
curtm4n <curtman.73@gmail.com>
2022-07-22 15:29:23 +0200

    Tout les services fonctionnels, projet presque fini :)

--- srcs/requirements/wordpress/tools/startup.sh
+++ srcs/requirements/wordpress/tools/startup.sh
@@ -5,6 +5,8 @@ then
 	mkdir /var/www/html/wordpress
 	cd /var/www/html/wordpress
 	wp core download --locale=fr_FR
+	wp config create --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD
+	wp core install --url=cdapurif.42.fr --title=mon_site --admin_user=$WORDPRESS_DB_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=curtis_cacao@hotmail.fr
 fi
 
 php-fpm7 -F -R
