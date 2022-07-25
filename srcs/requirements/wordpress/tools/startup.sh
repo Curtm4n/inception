e689b4f6201a59e46f918bdf8292817b659bafad
curtm4n <curtman.73@gmail.com>
2022-07-25 13:17:53 +0200

    Second user wp ajoute, projet termine

--- srcs/requirements/wordpress/tools/startup.sh
+++ srcs/requirements/wordpress/tools/startup.sh
@@ -8,6 +8,7 @@ then
 	wp core download --locale=fr_FR
 	wp config create --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD
 	wp core install --url=cdapurif.42.fr --title=mon_site --admin_user=$WORDPRESS_DB_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=curtis_cacao@hotmail.fr
+	wp user create correcteur correcteur@test.fr --user_pass=$WORDPRESS_DB_PASSWORD
 fi
 
 php-fpm7 -F -R
